fa1e1006748b15924dccc7c4464846ebd4fb8ce2
Deepthashash <36619790+Deepthashash@users.noreply.github.com>
2018-10-16 20:09:19 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int isEmpty(int x);
+int main()
+{
+	int maxsize = 8;
+	int stack[8];
+	int top=-1;
+	isEmpty(top);
+}
+
+int isEmpty(int x)
+{
+	if(x==-1) 
+		printf("Stack is empty");
+	else printf("Stack is not empty");
+}
+
+
+
+
+
